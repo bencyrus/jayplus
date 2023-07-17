9bf6e573ac9b0c579f2d8dd9106269dab8b82623
bencyrus <mahdi.mohaghegh2001@gmail.com>
2023-07-16 23:25:22 -0400

    updated config to contain auth config

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,23 +1,65 @@
 package config
 
 import (
+	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
 var (
-	DBHost     string
-	DBPort     string
-	DBName     string
-	DBUser     string
-	DBPassword string
-	Port       string
+	DBHost             string
+	DBPort             string
+	DBName             string
+	DBUser             string
+	DBPassword         string
+	Port               string
+	JWTIssuer          string
+	JWTAudience        string
+	JWTSecret          string
+	AccessTokenExpiry  time.Duration
+	RefreshTokenExpiry time.Duration
+	JWTCookieDomain    string
+	JWTCookiePath      string
+	JWTCookieName      string
 )
 
 func Load() {
+	LoadDBConfig()
+	LoadAppConfig()
+	LoadAuthConfig()
+}
+
+func LoadDBConfig() {
 	DBHost = os.Getenv("DB_HOST")
 	DBPort = os.Getenv("DB_PORT")
 	DBName = os.Getenv("DB_NAME")
 	DBUser = os.Getenv("DB_USER")
 	DBPassword = os.Getenv("DB_PASSWORD")
+}
+
+func LoadAppConfig() {
 	Port = os.Getenv("PORT")
+}
+
+func LoadAuthConfig() {
+	JWTIssuer = os.Getenv("JWT_ISSUER")
+	JWTAudience = os.Getenv("JWT_AUDIENCE")
+	JWTSecret = os.Getenv("JWT_SECRET")
+
+	AccessTokenExpiryInt, err := strconv.Atoi(os.Getenv("ACCESS_TOKEN_EXPIRY"))
+	if err != nil {
+		log.Fatalf("Error parsing ACCESS_TOKEN_EXPIRY: %v", err)
+	}
+	AccessTokenExpiry = time.Duration(AccessTokenExpiryInt) * time.Second
+
+	RefreshTokenExpiryInt, err := strconv.Atoi(os.Getenv("REFRESH_TOKEN_EXPIRY"))
+	if err != nil {
+		log.Fatalf("Error parsing REFRESH_TOKEN_EXPIRY: %v", err)
+	}
+	RefreshTokenExpiry = time.Duration(RefreshTokenExpiryInt) * time.Second
+
+	JWTCookieDomain = os.Getenv("JWT_COOKIE_DOMAIN")
+	JWTCookiePath = os.Getenv("JWT_COOKIE_PATH")
+	JWTCookieName = os.Getenv("JWT_COOKIE_NAME")
 }
--- backend/config/tests/config_test.go
+++ backend/config/tests/config_test.go
@@ -4,9 +4,10 @@ import (
 	"backend/config"
 	"os"
 	"testing"
+	"time"
 )
 
-func TestLoad(t *testing.T) {
+func TestLoadDBConfig(t *testing.T) {
 	// test cases
 	cases := []struct {
 		Name               string
@@ -16,7 +17,6 @@ func TestLoad(t *testing.T) {
 		ExpectedDBName     string
 		ExpectedDBUser     string
 		ExpectedDBPassword string
-		ExpectedPort       string
 	}{
 		{
 			Name: "All variables set",
@@ -26,14 +26,12 @@ func TestLoad(t *testing.T) {
 				"DB_NAME":     "test_db",
 				"DB_USER":     "test_user",
 				"DB_PASSWORD": "test_password",
-				"PORT":        "8080",
 			},
 			ExpectedDBHost:     "localhost",
 			ExpectedDBPort:     "5432",
 			ExpectedDBName:     "test_db",
 			ExpectedDBUser:     "test_user",
 			ExpectedDBPassword: "test_password",
-			ExpectedPort:       "8080",
 		},
 		{
 			Name:               "No variables set",
@@ -43,65 +41,157 @@ func TestLoad(t *testing.T) {
 			ExpectedDBName:     "",
 			ExpectedDBUser:     "",
 			ExpectedDBPassword: "",
-			ExpectedPort:       "",
-		},
-		{
-			Name: "Some variables set",
-			EnvVars: map[string]string{
-				"DB_HOST":     "localhost",
-				"DB_NAME":     "test_db",
-				"DB_PASSWORD": "test_password",
-			},
-			ExpectedDBHost:     "localhost",
-			ExpectedDBPort:     "",
-			ExpectedDBName:     "test_db",
-			ExpectedDBUser:     "",
-			ExpectedDBPassword: "test_password",
-			ExpectedPort:       "",
-		},
-		{
-			Name: "Only DB related variables set",
-			EnvVars: map[string]string{
-				"DB_HOST":     "localhost",
-				"DB_PORT":     "5432",
-				"DB_NAME":     "test_db",
-				"DB_USER":     "test_user",
-				"DB_PASSWORD": "test_password",
-			},
-			ExpectedDBHost:     "localhost",
-			ExpectedDBPort:     "5432",
-			ExpectedDBName:     "test_db",
-			ExpectedDBUser:     "test_user",
-			ExpectedDBPassword: "test_password",
-			ExpectedPort:       "",
 		},
 	}
 
-	// Iterate over test cases
 	for _, tc := range cases {
 		t.Run(tc.Name, func(t *testing.T) {
-			// Set environment variables
-			for k, v := range tc.EnvVars {
-				os.Setenv(k, v)
-			}
-
-			// Load config
-			config.Load()
-
-			// Check config values
-			if config.DBHost != tc.ExpectedDBHost ||
-				config.DBPort != tc.ExpectedDBPort ||
-				config.DBName != tc.ExpectedDBName ||
-				config.DBUser != tc.ExpectedDBUser ||
-				config.DBPassword != tc.ExpectedDBPassword ||
-				config.Port != tc.ExpectedPort {
-				t.Fatalf("Loaded config does not match expected config")
-			}
-
-			// Unset environment variables for next run
-			for k := range tc.EnvVars {
-				os.Unsetenv(k)
-			}
+			setEnvVars(tc.EnvVars)
+			config.LoadDBConfig()
+			checkDBConfig(t, tc)
+			unsetEnvVars(tc.EnvVars)
 		})
+	}
+}
+
+func TestLoadAppConfig(t *testing.T) {
+	// test cases
+	cases := []struct {
+		Name         string
+		EnvVars      map[string]string
+		ExpectedPort string
+	}{
+		{
+			Name: "Port set",
+			EnvVars: map[string]string{
+				"PORT": "8080",
+			},
+			ExpectedPort: "8080",
+		},
+		{
+			Name:         "Port not set",
+			EnvVars:      map[string]string{},
+			ExpectedPort: "",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			setEnvVars(tc.EnvVars)
+			config.LoadAppConfig()
+			checkAppConfig(t, tc)
+			unsetEnvVars(tc.EnvVars)
+		})
+	}
+}
+
+func TestLoadAuthConfig(t *testing.T) {
+	// test cases
+	cases := []struct {
+		Name                       string
+		EnvVars                    map[string]string
+		ExpectedJWTIssuer          string
+		ExpectedJWTAudience        string
+		ExpectedJWTSecret          string
+		ExpectedAccessTokenExpiry  time.Duration
+		ExpectedRefreshTokenExpiry time.Duration
+		ExpectedJWTCookieDomain    string
+		ExpectedJWTCookiePath      string
+		ExpectedJWTCookieName      string
+	}{
+		{
+			Name: "All variables set",
+			EnvVars: map[string]string{
+				"JWT_ISSUER":           "test_issuer",
+				"JWT_AUDIENCE":         "test_audience",
+				"JWT_SECRET":           "test_secret",
+				"ACCESS_TOKEN_EXPIRY":  "900",
+				"REFRESH_TOKEN_EXPIRY": "86400",
+				"JWT_COOKIE_DOMAIN":    "localhost",
+				"JWT_COOKIE_PATH":      "/",
+				"JWT_COOKIE_NAME":      "token",
+			},
+			ExpectedJWTIssuer:          "test_issuer",
+			ExpectedJWTAudience:        "test_audience",
+			ExpectedJWTSecret:          "test_secret",
+			ExpectedAccessTokenExpiry:  900 * time.Second,
+			ExpectedRefreshTokenExpiry: 86400 * time.Second,
+			ExpectedJWTCookieDomain:    "localhost",
+			ExpectedJWTCookiePath:      "/",
+			ExpectedJWTCookieName:      "token",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			setEnvVars(tc.EnvVars)
+			config.LoadAuthConfig()
+			checkAuthConfig(t, tc)
+			unsetEnvVars(tc.EnvVars)
+		})
+	}
+}
+
+func setEnvVars(vars map[string]string) {
+	for k, v := range vars {
+		os.Setenv(k, v)
+	}
+}
+
+func unsetEnvVars(vars map[string]string) {
+	for k := range vars {
+		os.Unsetenv(k)
+	}
+}
+
+func checkDBConfig(t *testing.T, tc struct {
+	Name               string
+	EnvVars            map[string]string
+	ExpectedDBHost     string
+	ExpectedDBPort     string
+	ExpectedDBName     string
+	ExpectedDBUser     string
+	ExpectedDBPassword string
+}) {
+	if config.DBHost != tc.ExpectedDBHost ||
+		config.DBPort != tc.ExpectedDBPort ||
+		config.DBName != tc.ExpectedDBName ||
+		config.DBUser != tc.ExpectedDBUser ||
+		config.DBPassword != tc.ExpectedDBPassword {
+		t.Fatalf("Loaded config does not match expected config")
+	}
+}
+
+func checkAppConfig(t *testing.T, tc struct {
+	Name         string
+	EnvVars      map[string]string
+	ExpectedPort string
+}) {
+	if config.Port != tc.ExpectedPort {
+		t.Fatalf("Loaded config does not match expected config")
+	}
+}
+
+func checkAuthConfig(t *testing.T, tc struct {
+	Name                       string
+	EnvVars                    map[string]string
+	ExpectedJWTIssuer          string
+	ExpectedJWTAudience        string
+	ExpectedJWTSecret          string
+	ExpectedAccessTokenExpiry  time.Duration
+	ExpectedRefreshTokenExpiry time.Duration
+	ExpectedJWTCookieDomain    string
+	ExpectedJWTCookiePath      string
+	ExpectedJWTCookieName      string
+}) {
+	if config.JWTIssuer != tc.ExpectedJWTIssuer ||
+		config.JWTAudience != tc.ExpectedJWTAudience ||
+		config.JWTSecret != tc.ExpectedJWTSecret ||
+		config.AccessTokenExpiry != tc.ExpectedAccessTokenExpiry ||
+		config.RefreshTokenExpiry != tc.ExpectedRefreshTokenExpiry ||
+		config.JWTCookieDomain != tc.ExpectedJWTCookieDomain ||
+		config.JWTCookiePath != tc.ExpectedJWTCookiePath ||
+		config.JWTCookieName != tc.ExpectedJWTCookieName {
+		t.Fatalf("Loaded config does not match expected config")
 	}
 }
