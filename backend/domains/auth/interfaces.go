8e1bc185688088269c4ec17aa9c1d80243b77d57
bencyrus <mahdi.mohaghegh2001@gmail.com>
2023-07-23 23:43:34 -0400

    updated interfaces

--- backend/domains/auth/interfaces.go
+++ backend/domains/auth/interfaces.go
@@ -6,9 +6,6 @@ import (
 )
 
 type AuthInterface interface {
-	GenerateSignedTokenPair(user *AuthUser) (JWTTokenPair, error)
-	GetRefreshCookie(refreshToken string) *http.Cookie
-	GetExpiredRefreshCookie() *http.Cookie
 	LoginHandler(w http.ResponseWriter, r *http.Request)
 	Authenticate(w http.ResponseWriter, r *http.Request, db db.DBInterface)
 }
